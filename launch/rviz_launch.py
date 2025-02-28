4227933884f8d7a36baee9bd6808b86fb3e59341
Adarsh Karan Kesavadas Prasanth <adarshkaran01@gmail.com>
2025-02-28 08:15:08 +0530

    Removed contributor tag from package.xml

--- launch/rviz_launch.py
+++ launch/rviz_launch.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Contributor: Adarsh Karan K P
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
