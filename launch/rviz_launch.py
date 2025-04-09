a5b433ea7e9744045e1582c596ed96b5f45d7466
Adarsh Karan Kesavadas Prasanth <112091438+AdarshKaran@users.noreply.github.com>
2025-04-09 15:20:40 +0530

    Refactored rviz launch file (#28)
    * Refactored rviz launch file
    
    * Removed contributor tag from package.xml
    
    * Added the start_rviz node to the navigation_neo.launch.py file and the necessary launch argument, removed changelog
    
    * Added the missing declare_use_rviz_cmd action in navigation_neo.launch.py

--- launch/rviz_launch.py
+++ launch/rviz_launch.py
@@ -12,45 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Contributor: Adarsh Karan K P
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
-from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_context import LaunchContext
+from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler, OpaqueFunction
+from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
 from launch.events import Shutdown
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 from nav2_common.launch import ReplaceString
 
-
-def generate_launch_description():
-    # Get the launch directory
-    bringup_dir = get_package_share_directory('neo_nav2_bringup')
-
-    # Create the launch configuration variables
-    namespace = LaunchConfiguration('namespace')
-    use_namespace = LaunchConfiguration('use_namespace')
-    rviz_config_file = LaunchConfiguration('rviz_config')
-
-    # Declare the launch arguments
-    declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace',
-        default_value='',
-        description=('Top-level namespace. The value will be used to replace the '
-                     '<robot_namespace> keyword on the rviz config file.'))
-
-    declare_use_namespace_cmd = DeclareLaunchArgument(
-        'use_namespace',
-        default_value='False',
-        description='Whether to apply a namespace to the navigation stack')
-
-    declare_rviz_config_file_cmd = DeclareLaunchArgument(
-        'rviz_config',
-        default_value=os.path.join(bringup_dir, 'rviz', 'single_robot.rviz'),
-        description='Full path to the RVIZ config file to use')
+def execution_stage(
+        context: LaunchContext,
+        namespace, 
+        use_namespace,
+        rviz_config_file,
+        rviz_output):
+    
+    rviz_output = str(rviz_output.perform(context))
+    
+    # When the rviz_output is log, stdout and stderr will not be shown in the terminal 
+    if rviz_output == 'log':
+        rviz_output_dict = {'both': 'log'}
+    else:
+        rviz_output_dict = {'both': 'screen'}
 
     namespaced_rviz_config_file = ReplaceString(
             source_file=rviz_config_file,
@@ -66,7 +57,7 @@ def generate_launch_description():
         condition=IfCondition(use_namespace),
         namespace=namespace,
         arguments=['-d', namespaced_rviz_config_file],
-        output='screen',
+        output=rviz_output_dict,
         )
 
     start_rviz_cmd = Node(
@@ -74,7 +65,7 @@ def generate_launch_description():
         executable='rviz2',
         condition=IfCondition(PythonExpression(['not ', use_namespace])),
         arguments=['-d', no_namespaced_rviz_config_file],
-        output='screen',
+        output=rviz_output_dict,
         )
 
     exit_event_handler_namespaced = RegisterEventHandler(
@@ -82,19 +73,54 @@ def generate_launch_description():
             target_action=start_namespaced_rviz_cmd,
             on_exit=EmitEvent(event=Shutdown(reason='rviz exited'))))
 
-    # Create the launch description and populate
-    ld = LaunchDescription()
+    launch_actions = [
+        start_namespaced_rviz_cmd,
+        start_rviz_cmd,
+        exit_event_handler_namespaced
+    ]
 
-    # Declare the launch options
-    ld.add_action(declare_namespace_cmd)
-    ld.add_action(declare_use_namespace_cmd)
-    ld.add_action(declare_rviz_config_file_cmd)
+    return launch_actions
 
-    # Add any conditioned actions
-    ld.add_action(start_namespaced_rviz_cmd)
-    ld.add_action(start_rviz_cmd)
+def generate_launch_description():
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description=('Top-level namespace. The value will be used to replace the '
+                     '<robot_namespace> keyword on the rviz config file.'))
 
-    # Add other nodes and processes we need
-    ld.add_action(exit_event_handler_namespaced)
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='False',
+        description='Whether to apply a namespace to the navigation stack')
 
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        'rviz_config',
+        default_value=os.path.join(
+            get_package_share_directory('neo_nav2_bringup'),
+            'rviz', 
+            'single_robot.rviz'),
+        description='Full path to the RVIZ config file to use')
+    
+    declare_rviz_output_cmd = DeclareLaunchArgument(
+        'rviz_output',
+        default_value='screen',
+        description='Output type for RViz node (screen or log)')
+
+    opq_function = OpaqueFunction(function=execution_stage,
+                              args=[
+                                  LaunchConfiguration('namespace'),
+                                  LaunchConfiguration('use_namespace'),
+                                  LaunchConfiguration('rviz_config'),
+                                  LaunchConfiguration('rviz_output')
+                              ])
+    
+    ld = LaunchDescription([
+        declare_namespace_cmd,
+        declare_use_namespace_cmd,
+        declare_rviz_config_file_cmd,
+        declare_rviz_output_cmd,
+        opq_function
+    ])
     return ld
+
